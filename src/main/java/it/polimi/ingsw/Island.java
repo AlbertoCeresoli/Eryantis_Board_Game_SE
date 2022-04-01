1c0b176bb30d1e8c03efb104fbaf993c74483ee2
Alberto Ceresoli <alberto.ceresoli@mail.polimi.it>
2022-04-01 15:10:16 +0200

    Island completed: To be Tested

--- src/main/java/it/polimi/ingsw/Island.java
+++ src/main/java/it/polimi/ingsw/Island.java
@@ -6,13 +6,33 @@ public class Island implements StudentManagement {
     private int[] students;
     private int inhibitionCards;
 
+    public Island() {
+        this.nTowers = 0;
+        this.students = new int[5];
+        this.inhibitionCards = 0;
+    }
+
+    public void removeTower(){
+        this.nTowers = 0;
+    }
+
+    public void addTower(int player, int towers){
+        this.controllerIndex = player;
+        this.nTowers = towers;
+    }
+
+    public void removeInhibitionCard(){
+        this.inhibitionCards--;
+    }
+
     @Override
     public void addStudents(int[] students) {
-
+        for (int i = 0; i < students.length; i++){
+            this.students[i] = students[i];
+        }
     }
 
     @Override
     public void removeStudents(int[] students) {
-
     }
 }
