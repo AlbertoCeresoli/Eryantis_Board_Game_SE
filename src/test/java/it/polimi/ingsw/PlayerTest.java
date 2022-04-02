cfbdd3bc9f21c5cde2e4ca78379f10aee1d698cc
alecalvi00 <alessandro.calvi1@gmail.com>
2022-04-02 12:39:40 +0200

    improvement of "Player" class and implementation of TestPlayer

---
+++ src/test/java/it/polimi/ingsw/PlayerTest.java
@@ -0,0 +1,58 @@
+package it.polimi.ingsw;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PlayerTest {
+    Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = new Player();
+    }
+
+    @Test
+    @DisplayName("simple cases")
+    void testFixHand(){
+        int [][] assistantsNow = {{2,1,2,2,2,2,2,2,2,2}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 1, 2, 2, 3, 3, 4, 4, 5, 5}};
+        assertTrue(player.fixHand(1), "card played");
+        assertArrayEquals(assistantsNow, player.getAssistants(), "card played correctly");
+    }
+
+    @Test
+    @DisplayName("case with an already played card")
+    void testFixHandCardAlreadyPlayed(){
+        int [][] assistantsNow = {{2,1,2,2,2,2,2,2,2,2}, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, {1, 1, 2, 2, 3, 3, 4, 4, 5, 5}};
+        assertTrue(player.fixHand(1), "card played");
+        assertArrayEquals(assistantsNow, player.getAssistants(), "card played correctly");
+        assertFalse(player.fixHand(1), "card already played");
+        assertArrayEquals(assistantsNow, player.getAssistants(), "card played correctly");
+    }
+
+    @Test
+    @DisplayName("simple case")
+    void testRemoveCoins(){
+        int testCoins=2;
+        for (int i=0; i<testCoins-1; i++) {
+            player.addCoin();
+        }
+        assertTrue(player.removeCoins(1), "coin removed");
+        assertEquals(1, player.getCoins());
+    }
+
+    @Test
+    @DisplayName("case with not enough coins")
+    void testOverloadRemoveCoins(){
+        int testCoins=4;
+        for (int i=0; i<testCoins-1; i++) {
+            player.addCoin();
+        }
+        assertTrue(player.removeCoins(1), "coin removed");
+        assertEquals(3, player.getCoins(),"coins removed correctly");
+        assertFalse(player.removeCoins(5), "not enough coins");
+        assertEquals(3, player.getCoins(), "coins not removed");
+    }
+}
